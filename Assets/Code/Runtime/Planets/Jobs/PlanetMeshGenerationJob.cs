d22bb82807dd33fed815d76226ad77bd1bd644fe
UknownPI <lepro.pablo@gmail.com>
2024-07-10 20:56:56 +0200

    Some More Changes - Nothing Special
    Moved a bit of what the job did to the MonoBehaviour script , and basically everything I did so far , including commented things that don't work well and some very long comments of me wandering what I should do and how I should do it.

--- Assets/Code/Runtime/Planets/Jobs/PlanetMeshGenerationJob.cs
+++ Assets/Code/Runtime/Planets/Jobs/PlanetMeshGenerationJob.cs
@@ -28,6 +28,7 @@ namespace PLE.Prototype.Runtime.Code.Runtime.Planets.Jobs
     {
         public NativeList<Vertex> Vertices;
         public NativeList<Triangle> Triangles;
+        public NativeHashMap<float3, int> VertexToIndex;
 
         public float cube;
         public int MaxIteration;
@@ -35,163 +36,99 @@ namespace PLE.Prototype.Runtime.Code.Runtime.Planets.Jobs
         public Vector3 campos;
         public Vector3 selfposition;
 
+        public float floorheight;
         public float strenght;
         public float roughtness;
         public float3 center;
+        public float3 centerOffset;
         public int layernumber;
         public float roughtnesschange;
         public float strenghtchange;
 
-        public bool doBissect(float distance,int iteration)
+        public bool doBissect(float distance, int iteration)
         {
-            // I could use a better structure like a dictionnary defined in a start 
-            if ((iteration <= 5 ||  // Do 5 iteration for everything
-                (distance < 32 && iteration <= 6) || // Do 6 iteration for distance < 6
-                (distance < 16 && iteration <= 7) || // Do 7 iteration for distance < 5
-                (distance < 8 && iteration <= 8) || 
-                (distance < 4 && iteration <= 9) ||
-                (distance < 2 && iteration <= 10) || 
-                (distance < 1.00 && iteration <= 11) || 
-                (distance < 0.5 && iteration <= 12) || 
-                (distance < 0.25 && iteration <= 13) || 
-                (distance < 0.125 && iteration <= 14)))
-            { return true; }else { return false; }
+            // I could use a better structure like a dictionnary, or maybe just a (mathematical) function
+             return (                    iteration <= 5 ) || // Do 5 iteration for everything
+                    (distance < 16    && iteration <= 6 ) || // Do 6 iteration for distance < 32
+                    (distance < 8     && iteration <= 7 ) || // Do 7 iteration for distance < 16
+                    (distance < 4     && iteration <= 8 ) ||
+                    (distance < 2     && iteration <= 9 ) ||
+                    (distance < 1     && iteration <= 10) ||
+                    (distance < 0.500 && iteration <= 11) ||
+                    (distance < 0.250 && iteration <= 12) ||
+                    (distance < 0.125 && iteration <= 13) ||
+                    (distance < 0.0625 && iteration <= 14);
         }
         public void Execute()
-        {
-            selfposition = float3.zero;
+        {           
 
-            // TOP   FRONT  BACK    RIGHT   LEFT (Views)
-            // 20    02     64      40      26  
-            // 64    13     75      51      37
-            Vertices.Add(new Vertex { Position = math.normalize(new float3(+1,+1,+1)) });//0
-            Vertices.Add(new Vertex { Position = math.normalize(new float3(+1,-1,+1)) });//1
-
-            Vertices.Add(new Vertex { Position = math.normalize(new float3(-1,+1,+1)) });//2
-            Vertices.Add(new Vertex { Position = math.normalize(new float3(-1,-1,+1)) });//3
-
-            Vertices.Add(new Vertex { Position = math.normalize(new float3(+1,+1,-1)) });//4
-            Vertices.Add(new Vertex { Position = math.normalize(new float3(+1,-1,-1)) });//5
-
-            Vertices.Add(new Vertex { Position = math.normalize(new float3(-1,+1,-1)) });//6
-            Vertices.Add(new Vertex { Position = math.normalize(new float3(-1,-1,-1)) });//7
-
-            for (int id = 0; id < Vertices.Length; id++)
-            {
-                var x = Vertices[id];
-                x.Position = changeHeight(x.Position);
-                Vertices[id] = x;
-            }
-
-            var vertexDict = new NativeHashMap<float3, int>(23000, Allocator.TempJob); // This "dict" or whatever a hashmap is is connecting vertex positions to their index in verticies (so that I check for duplicated verticies and delete them)
-            int T =0;
-            foreach (var thing in Vertices) { vertexDict[thing.Position] = T; T++; }
-            // TOP
-            Triangles.Add(new Triangle { Index0 = (short)4, Index1 = (short)2, Index2 = (short)0});
-            Triangles.Add(new Triangle { Index0 = (short)2, Index1 = (short)4, Index2 = (short)6});
-            // BOT
-            Triangles.Add(new Triangle { Index0 = (short)3, Index1 = (short)5, Index2 = (short)1});
-            Triangles.Add(new Triangle { Index0 = (short)5, Index1 = (short)3, Index2 = (short)7});
-            // FRONT
-            Triangles.Add(new Triangle { Index0 = (short)3, Index1 = (short)0, Index2 = (short)2 });
-            Triangles.Add(new Triangle { Index0 = (short)0, Index1 = (short)3, Index2 = (short)1 });
-            // BACK
-            Triangles.Add(new Triangle { Index0 = (short)5, Index1 = (short)6, Index2 = (short)4 });
-            Triangles.Add(new Triangle { Index0 = (short)6, Index1 = (short)5, Index2 = (short)7 });
-            // RIGHT
-            Triangles.Add(new Triangle { Index0 = (short)1, Index1 = (short)4, Index2 = (short)0 });
-            Triangles.Add(new Triangle { Index0 = (short)4, Index1 = (short)1, Index2 = (short)5 });
-            // Left
-            Triangles.Add(new Triangle { Index0 = (short)7, Index1 = (short)2, Index2 = (short)6 });
-            Triangles.Add(new Triangle { Index0 = (short)2, Index1 = (short)7, Index2 = (short)3 });
-
-            
-
-            
-
-            for (int i=0; i < MaxIteration; i++) { 
+            for (int i=0; i < MaxIteration; i++) {
                 int l = Triangles.Length;
                 for (int index = 0; index < l; index++)
                 {
                     float3 pa = Vertices[Triangles[index].Index0].Position;
                     float3 pb = Vertices[Triangles[index].Index1].Position;
                     float3 pc = Vertices[Triangles[index].Index2].Position;
+                    // now that i think about it this normalization might not be usefull at all
+                    float3 m = math.normalize((pa + pb + pc) / 3); // middle point / average (The first few itarations have points real far away , and taking the average is important (we might be able to skip this for higher iterations when triangles are smaller))
+                    if ((math.dot(campos, m) < -0.1) && (i > 4)) { continue; }  // Dont divide back side (backside is  dot()<0 but we might want a little bit of the back side for the triangles near the back side that share verticies of the backside)
 
-                    float3 m = math.normalize((pa + pb + pc) / 3); // middle point / average (The first few itarations have points real far away , and taking the average is important (we might be able to skip this for higher iterations when Triangles are smaller))
-                    if ((math.dot(campos, m) < -0.1)&& (i > 4)) {continue; }  // Dont divide back side (backside is  dot()<0 but we might want a little bit of the back side for the Triangles near the back side that share verticies of the backside)
-
-                     
-                    float3 newp = math.normalize((float3)((pa + pb) / 2)); // Point that splits Triangles     
+                    Vector3 newp = math.normalize((pa + pb) / 2); // Point that splits triangles     
                     newp = changeHeight(newp);
-                    float d = (campos - new Vector3(newp.x, newp.y, newp.z)).magnitude;// distance
+
+                    float d = (campos - newp).magnitude;// distance
                     // If the triangel is already devided devide it (if there is a point in the middle of the hypotenus) if not then create the new point and then divide it
+
+                    // we check if point have been created , but if we create a new point that point might appear on a triangle that have already been looped on so that triangle wont take it in account 
+                    // we could check for the triangle sharing Index0 and Index1 but we'd need a dict that assosiates points to multiple triangles .. but that seems to much. (need a lot of ressources and time)
+
+                    // If we want to get rid of the T shaped emm... holes ? We could assign 1 interger to each vertices that correspond to the LOWEST level of iteration that this point is part of (between the 4 triangles that share him) , and we could make it so that we only divide a triangle if both of his points (we dont care about the 3rd one) have a level of iteration just one layer above the one currently doin' 
                     int p;
-                    if (!vertexDict.TryGetValue(newp, out p))
+                    if(true) //(!VertexToIndex.TryGetValue(newp, out p))
                     {
-                        if (!doBissect(distance: d, iteration: i)) {continue;} // If dont generate then dont
+                        // IMPORTANT TO DO
+                        // I need a systeme where sometimes it ignores the dot() depending on how many levels it should be displaying 
+                        // ADDING THIS CAN HELP A LOT PERFORMANCES BUT GOOD LUCK MAKING THIS WORK **WELL** (it's supose to detect if a point is before or  after the horizon)
+                        //if (math.dot(campos - newp, newp) < v1 && (i > v2)) { continue; } //v1 default = 0 , v2 default somthing like 4 
+                        if (!doBissect(distance: d, iteration: i)) { continue; } // If dont generate then dont
+
+                        // if we dont care about camera distance and that ALL triangles are cut in half then we woudn't need an dict to find out if point have been created , we know if it have been or not depending if we are in the first or second half of the loop
+                        // and the place of the point is also easy to find 
+                        // ALSO if I used a structure that holds empty slots for places where there could be an point we could also find easily the point without a dict (something like a binary tree i think) - from (1.1,2,1.2) to (1,1,1,0) I just dont know if using much bigger varrialbe with lots of empty values is a good idea
                         p = Vertices.Length;
-                        vertexDict[newp] = p;
-                        Vertices.Add(new Vertex { Position = newp }) ;//6
+                        VertexToIndex[newp] = p;
+                        Vertices.Add(new Vertex { Position = newp });
                     }
-                    // We need to destroy the old triangle and add 2 new Triangles 
-                    Triangles.Add(new Triangle { Index0 = Triangles[index].Index1, Index1 = Triangles[index].Index2, Index2 = (short)p });
+                    // We need to destroy (write over 1) the old triangle and add 2 new triangles (add 1) 
+                    Triangles.Add(     new Triangle { Index0 = Triangles[index].Index1, Index1 = Triangles[index].Index2, Index2 = (short)p });
                     Triangles[index] = new Triangle { Index1 = Triangles[index].Index0, Index0 = Triangles[index].Index2, Index2 = (short)p };
+
                 }
             }
         }
 
-        float noise(float3 point)
-        {            
-            return (Mathf.PerlinNoise(point.x, point.y) +Mathf.PerlinNoise(point.y, point.x) +Mathf.PerlinNoise(point.z, point.y) + Mathf.PerlinNoise(point.y, point.z) + Mathf.PerlinNoise(point.z, point.x) + Mathf.PerlinNoise(point.x, point.z)) / 6;
+        private float PerlinNoise3D(float3 point)
+        {
+            return (Mathf.PerlinNoise(point.x, point.y) + Mathf.PerlinNoise(point.y, point.x) + Mathf.PerlinNoise(point.z, point.y) + Mathf.PerlinNoise(point.y, point.z) + Mathf.PerlinNoise(point.z, point.x) + Mathf.PerlinNoise(point.x, point.z)) / 6;
         }
 
-
-        float3 changeHeight(float3 point)
+        private float3 changeHeight(float3 point)
         {
+
             float noiseToAdd = 0;
             float r = roughtness;
             float s = strenght;
+            float3 c = center;
             for (int i = 0; i < layernumber; i++)
             {
-                noiseToAdd += noise(point * r + center) * s;
+                noiseToAdd += PerlinNoise3D(point * r + c) * s;
                 r *= roughtnesschange;
                 s *= strenghtchange;
+                c += centerOffset; // change of center -> its acttualy more of a sample point , this change could be done in other ways
             }
+            noiseToAdd = Mathf.Max(0, noiseToAdd - floorheight); // set changes to 0 if they are not big enoth (result in a "ground" layer , an ocean floor)
             return (noiseToAdd + 1) * Vector3.Normalize(point) * 1;
-        }
-        void SetHeights(int id,ref NativeList<Vertex> Vertices)
-        {
-            var p = Vertices[id];
-            var pp = p.Position;
-
-            /* Craters
-            //p.Position = new float3(pp.x * (1+Mathf.Sin(pp.y*v1 + v2)*v3),pp.y ,pp.z * (1 + Mathf.Sin(pp.y * v1 +v2) * v3)) ;
-            float3 ppp = v4 - pp;
-            float3 pppp = (Vector3)v4 - (Vector3.Normalize(ppp) * v1);
-
-            //while (Vector3.Magnitude(ppp) < v1) { pp *= 0.9f; ppp = v4 - pp; }
-            if (Vector3.Magnitude(ppp) < v1) { p.Position =pppp; p.Normal = (Vector3.Normalize((Vector3)(v4 - p.Position))); p.UV = new float2(p.Position.x, p.Position.z); }
-            */
-            float noiseToAdd = 0;
-            float r = roughtness;
-            float s = strenght;
-            for (int i = 0; i < layernumber; i++)
-            {
-                noiseToAdd += noise(pp * r + center) * s;
-                r*= roughtnesschange;
-                s *= strenghtchange;
-            }
-            p.Position = (noiseToAdd+1) * p.Position * 1; // 1= radius
-            /*
-            noiseToAdd +=noise(pp * varriable1,p1) * influences1; // 
-            // if (multiplier>v3)
-            noiseToAdd += Mathf.Pow((1-Mathf.Abs(noise(pp* varriable2,p2))),v2) * influences2; // Rigid Noise, not working
-
-            p.Position = Vector3.Normalize(p.Position) * (Vector3.Magnitude(p.Position) + noiseToAdd); // Add Noise
-            if (Vector3.Magnitude(p.Position) < v1) { p.Position = Vector3.Normalize(p.Position) * v1; } // Minimum floor
-            */
-            //p.UV = new Vector2(Vector3.Magnitude(p.Position)-0.75f, Vector3.Magnitude(p.Position) - 0.75f); // This isnt real well done -> Setting uv according to height
-            Vertices[id] = p;
         }
     }
 }
+
--- Assets/Code/Runtime/Planets/PlanetMeshGenerationTest.cs
+++ Assets/Code/Runtime/Planets/PlanetMeshGenerationTest.cs
@@ -30,10 +30,12 @@ namespace PLE.Prototype.Runtime.Code.Runtime.Planets
         public int MaxIteration;
         public Transform cameraPosition;
         private Vector3 lastpos;
+        public float floorheight;
         public parameters parameter;
 
         private NativeList<Vertex> vertices;
         private NativeList<Triangle> triangles;
+        private NativeHashMap<float3, int> vertexToIndex;
 
         [System.Serializable]
         public class parameters
@@ -50,6 +52,7 @@ namespace PLE.Prototype.Runtime.Code.Runtime.Planets
             public float strenghtchange;
 
             public float3 center;
+            public float3 centerOffset;
         }
         private void Start() {
             previewGameObject = new GameObject(nameof(PlanetMeshGenerationTest));
@@ -58,6 +61,7 @@ namespace PLE.Prototype.Runtime.Code.Runtime.Planets
 
             vertices = new NativeList<Vertex>(Allocator.Persistent);
             triangles = new NativeList<Triangle>(Allocator.Persistent);
+            vertexToIndex = new NativeHashMap<float3, int>(23000, Allocator.Persistent);
         }
 
         private void OnDestroy()
@@ -73,6 +77,7 @@ namespace PLE.Prototype.Runtime.Code.Runtime.Planets
 
             vertices.Dispose();
             triangles.Dispose();
+            vertexToIndex.Dispose();
         }
 
         private unsafe void Update()
@@ -82,6 +87,9 @@ namespace PLE.Prototype.Runtime.Code.Runtime.Planets
             
             vertices.Clear();
             triangles.Clear();
+            vertexToIndex.Clear();
+
+            initializeVariables(triangles: ref triangles,vertices: ref vertices, vertexToIndex: ref vertexToIndex);
 
             //EntityManager manager = World.DefaultGameObjectInjectionWorld.EntityManager;
             var job = new PlanetMeshGenerationJob
@@ -95,7 +103,11 @@ namespace PLE.Prototype.Runtime.Code.Runtime.Planets
                 center = parameter.center,  
                 layernumber = parameter.layernumber,
                 roughtnesschange = parameter.roughtnesschange,
-                strenghtchange = parameter.strenghtchange
+                strenghtchange = parameter.strenghtchange,
+                centerOffset = parameter.centerOffset,
+                selfposition = float3.zero,
+                VertexToIndex = vertexToIndex,
+                floorheight = floorheight
             };
             job.Run();
             
@@ -150,6 +162,87 @@ namespace PLE.Prototype.Runtime.Code.Runtime.Planets
                 [2] = new(VertexAttribute.Tangent, VertexAttributeFormat.Float32, 4),
                 [3] = new(VertexAttribute.TexCoord0, VertexAttributeFormat.Float32, 2),
             };
+        }
+
+        private void initializeVariables(ref NativeList<Triangle> triangles, ref NativeList<Vertex> vertices, ref NativeHashMap<float3,int> vertexToIndex) {
+
+
+            setDefaultCube(ref triangles, ref vertices);
+
+            for (int id = 0; id < vertices.Length; id++)
+            {
+                Vertex x = vertices[id];
+                x.Position = changeHeight(x.Position);
+                vertices[id] = x;
+                vertexToIndex[x.Position] = id;
+            }
+        }
+
+        private void setDefaultCube(ref NativeList<Triangle> triangles, ref NativeList<Vertex> vertices)
+        {
+            // TOP   FRONT  BACK    RIGHT   LEFT (Views)
+            // 20    02     64      40      26  
+            // 64    13     75      51      37
+
+            vertices.Add(new Vertex { Position = math.normalize(new float3(+1, +1, +1)) });//0
+            vertices.Add(new Vertex { Position = math.normalize(new float3(+1, -1, +1)) });//1
+
+            vertices.Add(new Vertex { Position = math.normalize(new float3(-1, +1, +1)) });//2
+            vertices.Add(new Vertex { Position = math.normalize(new float3(-1, -1, +1)) });//3
+
+            vertices.Add(new Vertex { Position = math.normalize(new float3(+1, +1, -1)) });//4
+            vertices.Add(new Vertex { Position = math.normalize(new float3(+1, -1, -1)) });//5
+
+            vertices.Add(new Vertex { Position = math.normalize(new float3(-1, +1, -1)) });//6
+            vertices.Add(new Vertex { Position = math.normalize(new float3(-1, -1, -1)) });//7
+            /*
+            // TOP
+            triangles.Add(new Triangle { Index0 = (short)4, Index1 = (short)2, Index2 = (short)0 }); // top1
+            triangles.Add(new Triangle { Index0 = (short)2, Index1 = (short)4, Index2 = (short)6 }); // top2
+            // BOT
+            triangles.Add(new Triangle { Index0 = (short)3, Index1 = (short)5, Index2 = (short)1 }); // bot1
+            triangles.Add(new Triangle { Index0 = (short)5, Index1 = (short)3, Index2 = (short)7 }); // bot2 
+            // FRONT
+            triangles.Add(new Triangle { Index0 = (short)3, Index1 = (short)0, Index2 = (short)2 }); // front1
+            triangles.Add(new Triangle { Index0 = (short)0, Index1 = (short)3, Index2 = (short)1 }); // front2
+            // BACK
+            triangles.Add(new Triangle { Index0 = (short)5, Index1 = (short)6, Index2 = (short)4 });
+            triangles.Add(new Triangle { Index0 = (short)6, Index1 = (short)5, Index2 = (short)7 });
+            // RIGHT
+            triangles.Add(new Triangle { Index0 = (short)1, Index1 = (short)4, Index2 = (short)0 });
+            triangles.Add(new Triangle { Index0 = (short)4, Index1 = (short)1, Index2 = (short)5 });
+            // Left
+            triangles.Add(new Triangle { Index0 = (short)7, Index1 = (short)2, Index2 = (short)6 });
+            triangles.Add(new Triangle { Index0 = (short)2, Index1 = (short)7, Index2 = (short)3 });*/
+
+
+            // This configuration (top1, bot1 , front1 .... left1, top2 , bot2 ... left2) will make it that the first half of the list have same "direction", later when cutting them in half (as long as we cut all of them in half (without worring about the camera distance)) the first half of the list will need the creation of a new point and the second half will not need to add a new point but will use the point created by the first half, (so if we run the code in parallele , the first half will edit the vertices list and the second half will not and will need the first half to have been ran first)
+            triangles.Add(new Triangle { Index0 = (short)4, Index1 = (short)2, Index2 = (short)0 }); triangles.Add(new Triangle { Index0 = (short)3, Index1 = (short)5, Index2 = (short)1 }); triangles.Add(new Triangle { Index0 = (short)3, Index1 = (short)0, Index2 = (short)2 }); triangles.Add(new Triangle { Index0 = (short)5, Index1 = (short)6, Index2 = (short)4 }); triangles.Add(new Triangle { Index0 = (short)1, Index1 = (short)4, Index2 = (short)0 }); triangles.Add(new Triangle { Index0 = (short)7, Index1 = (short)2, Index2 = (short)6 });
+            triangles.Add(new Triangle { Index0 = (short)2, Index1 = (short)4, Index2 = (short)6 }); triangles.Add(new Triangle { Index0 = (short)5, Index1 = (short)3, Index2 = (short)7 }); triangles.Add(new Triangle { Index0 = (short)0, Index1 = (short)3, Index2 = (short)1 }); triangles.Add(new Triangle { Index0 = (short)6, Index1 = (short)5, Index2 = (short)7 }); triangles.Add(new Triangle { Index0 = (short)4, Index1 = (short)1, Index2 = (short)5 }); triangles.Add(new Triangle { Index0 = (short)2, Index1 = (short)7, Index2 = (short)3 });
+        }
+
+        // idk if I should do this or if I should modify the function in the job and call it from here
+        private float PerlinNoise3D(float3 point)
+        {
+            return (Mathf.PerlinNoise(point.x, point.y) + Mathf.PerlinNoise(point.y, point.x) + Mathf.PerlinNoise(point.z, point.y) + Mathf.PerlinNoise(point.y, point.z) + Mathf.PerlinNoise(point.z, point.x) + Mathf.PerlinNoise(point.x, point.z)) / 6;
+        }
+
+        private float3 changeHeight(float3 point)
+        {
+
+            float noiseToAdd = 0;
+            float r = parameter.roughtness;
+            float s = parameter.strenght;
+            float3 c = parameter.center;
+            for (int i = 0; i < parameter.layernumber; i++)
+            {
+                noiseToAdd += PerlinNoise3D(point * r + c) * s;
+                r *= parameter.roughtnesschange;
+                s *= parameter.strenghtchange;
+                c += parameter.centerOffset;
+            }
+            noiseToAdd = Mathf.Max(0, noiseToAdd - floorheight);
+            return (noiseToAdd + 1) * Vector3.Normalize(point) * 1;
         }
     }
 }
